459f1954a4fe58f3246f098818d169141af334e4
Flare <Otagaisakana@gmail.com>
2023-07-26 22:43:39 -0400

    Info command altered to separate testing server data from global data

--- commands/immersion-commands/info.js
+++ commands/immersion-commands/info.js
@@ -2,6 +2,7 @@ const { SlashCommandBuilder } = require('discord.js');
 const { mongoose } = require('mongoose');
 const User = require("../../models/User");
 const Log = require("../../models/Log");
+const { testingServerId } = require('../../config.json');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -9,11 +10,22 @@ module.exports = {
         .setDescription('Replies with your immersion info!'),
     async execute(interaction) {
         const userId = interaction.user.id;
+        const guildId = interaction.guild.id;
         const exists = await User.exists({ userId: userId });
         let totalPoints = 0;
 
+        // Exclude testing server data
+        let testGuildExcludeMatch;
+        if (guildId === testingServerId) {
+            testGuildExcludeMatch = { $match: { guildId: testingServerId} }
+        } else {
+            testGuildExcludeMatch = { $match: { guildId: { $ne: testingServerId } } }
+        };
+
+
         if (exists) {
             totalPoints = await Log.aggregate([
+                testGuildExcludeMatch,
                 { $match: { userId: userId } },  // Only consider logs for this user
                 { $group: { _id: "$userId", totalPoints: { $sum: "$points" } } },
                 { $project: { totalPoints: 1, _id: 0 } }  // Only return totalPoints
