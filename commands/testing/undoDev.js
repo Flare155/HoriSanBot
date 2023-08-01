d99e4cfa1790c2ed768c669ef36980b728d49530
Flare <Otagaisakana@gmail.com>
2023-08-01 01:09:34 -0400

    Changed undo command name to lower case

--- commands/testing/undoDev.js
+++ commands/testing/undoDev.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('undo_Dev')
+		.setName('undo_dev')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
 		await interaction.reply('Pong!');
