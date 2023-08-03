2f555af16308982452e62a786d6ca2daea03732b
Flare <Otagaisakana@gmail.com>
2023-08-03 01:44:46 -0400

    I think it works now fr

--- commands/global/leaderboard.js
+++ commands/global/leaderboard.js
@@ -47,12 +47,28 @@ module.exports = {
             case 'All Time':
                 startDate = new Date(0); // Beginning of Unix time
                 break;
+            case 'Yearly':
+                // For 'Yearly', we want to start from the beginning of the current year.
+                // We set the month and date to their minimum values (0 for January and 1 for the first day).
+                startDate = new Date(Date.UTC(now.getUTCFullYear(), 0, 1)); // Start of this year
+                break;
+            case 'Monthly':
+                // For 'Monthly', we want to start from the beginning of the current month.
+                // We set the date to its minimum value (1 for the first day).
+                startDate = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), 1)); // Start of this month
+                break;
+            case 'Weekly':
+                // For 'Weekly', we want to start from the beginning of the current week.
+                // We're using the getUTCDay() function, which returns the day of the week (0 for Sunday, 1 for Monday, etc.).
+                // By subtracting this from the current date, we get the last Sunday.
+                startDate = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate() - now.getUTCDay())); // Start of this week
+                break;
             case 'Today':
                 // For 'Today', we just want to start from the beginning of the current day.
-                // We use UTC to avoid timezone issues.
                 startDate = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate())); // Start of today
                 break;
         };
+        
         
 
         // Build $match stage
