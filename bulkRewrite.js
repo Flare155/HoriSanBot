6375bfff81d6646fe06d149267f8ff0f7978d2a0
Flare155 <Otagaisakana@gmail.com>
2024-10-07 02:58:37 -0400

    changed deletelog command name

--- bulkRewrite.js
+++ bulkRewrite.js
@@ -4,32 +4,32 @@ const Log = require('./models/Log');
 
 console.log("Hello   World");
 
-// Get all logs that are of the unit "Chars"
+// Get all logs that are of the unit "Pages"
 
 // Go through one by one, divide points by 2 and save
 
 // Done
 
-(async () => {
-    // Connect to DB
-    await mongoose.connect(AtlasDbUrl)
+// (async () => {
+//     // Connect to DB
+//     await mongoose.connect(AtlasDbUrl)
 
-    // Get all logs that are of the unit "Chars"
-    let allLogs = await Log.find({ unit: "Pages" }).exec();;
-    console.log(allLogs.length);
+//     // Get all logs that are of the unit "Pages"
+//     let allLogs = await Log.find({ unit: "Pages" }).exec();;
+//     console.log(allLogs.length);
     
-    // Go through 1 by 1 devide points by 2 and save
-    const updateQueries = [];
-    allLogs.forEach(async (item) => {
-        updateQueries.push({
-          updateOne: {
-            filter: { _id: item._id },
-            update: { points: item.points/4 },
-          },
-        });
-      });
-    // console.log(JSON.stringify(updateQueries));
-    await Log.bulkWrite(updateQueries);
-})()
+//     // Go through 1 by 1 devide points by 4 and save
+//     const updateQueries = [];
+//     allLogs.forEach(async (item) => {
+//         updateQueries.push({
+//           updateOne: {
+//             filter: { _id: item._id },
+//             update: { points: item.points/4 },
+//           },
+//         });
+//       });
+//     // console.log(JSON.stringify(updateQueries));
+//     await Log.bulkWrite(updateQueries);
+// })()
 
 
--- commands/global/deleteLog.js
+++ commands/global/deleteLog.js
@@ -4,7 +4,7 @@ const Log = require('../../models/Log');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('delete_log')
+		.setName('deleteLog')
 		.setDescription('Deletes a specific log by its ID.')
 		.addStringOption(option =>
 			option.setName('log_id')
