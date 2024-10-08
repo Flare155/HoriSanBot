d7c6a5282508603112e9fae6eb3b424a231019c4
Flare155 <Otagaisakana@gmail.com>
2024-10-07 20:27:29 -0400

    added local time converter utility

---
+++ utils/localTimeConverter.js
@@ -0,0 +1,12 @@
+const localTimeConverter = (date, timezone) => {
+  return new Intl.DateTimeFormat('en-US', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    timeZone: timezone
+  }).format(new Date(date));
+};
+
+module.exports = { localTimeConverter};
