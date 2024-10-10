810470b1dbdd4fa5cd761084d98df93fcf6fba43
Flare155 <Otagaisakana@gmail.com>
2024-10-10 12:11:08 -0400

    status display bug fixes

--- events/setStatus.js
+++ events/setStatus.js
@@ -1,18 +1,22 @@
-const { Events } = require('discord.js');
+const { Events, ActivityType } = require('discord.js');
 
 module.exports = {
-    name: Events.ClientReady,
-    once: true,
-    execute(client) {
-        const serverCount = client.guilds.cache.size;
-        let status = { name: `/help, active in ${serverCount} servers`, type: 'CUSTOM' };
+  name: Events.ClientReady,
+  once: true,
+  execute(client) {
+    const serverCount = client.guilds.cache.size;
 
-        // Set the presence with custom status
-        client.user.setPresence({
-            activities: [status], // Set the status
-            status: 'online', // Options: 'online', 'idle', 'dnd', 'invisible'
-        });
+    client.user.setPresence({
+      activities: [
+        {
+          name: 'Custom Status', // Provide a string for the name
+          type: ActivityType.Custom,
+          state: `/help • Active in ${serverCount} servers`,
+        },
+      ],
+      status: 'online', // Options: 'online', 'idle', 'dnd', 'invisible'
+    });
 
-        console.log(`Status set to: /help, active in ${serverCount} servers`);
-    },
+    console.log(`Status set to: /help | Active in ${serverCount} servers`);
+  },
 };
