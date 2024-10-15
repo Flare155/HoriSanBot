0304f035bdfb21fadc3e95989dbfb9ede46881ab
Philip Davis <philip29313@hotmail.com>
2024-10-15 03:27:22 -0400

    added mini repro

---
+++ mini.js
@@ -0,0 +1,20 @@
+const puppeteer = require("puppeteer");
+
+const a = async () => {
+    const browser = await puppeteer.launch({
+    headless: true,
+    defaultViewport: null,
+    args: [
+        '--disable-web-security',
+        '--disable-features=IsolateOrigins',
+        '--disable-site-isolation-trials',
+        '--no-sandbox',
+        '--remote-debugging-port=9222'
+    ],
+    executablePath: process.platform == "linux" ? '/usr/bin/chromium' : null, // Use an environment variable instead of checking the OS
+    });
+
+console.log("✅ Browser initialised.");
+}
+
+a();
