e469d25525b721ae9bc86a9551ac9d7e4e47bd9c
Flare155 <Otagaisakana@gmail.com>
2024-10-15 18:27:57 -0400

    updated browser file

--- events/browser.js
+++ events/browser.js
@@ -1,25 +1,27 @@
-const { default: puppeteer } = require("puppeteer");
+const puppeteer = require('puppeteer'); // Import puppeteer (bundled with Chromium)
 const { Events } = require('discord.js');
-
-
 
 module.exports = {
     name: Events.ClientReady,
     once: true,
     async execute() {
-        const browser = await puppeteer.launch({
-            headless: true,
-            defaultViewport: null,
-            args: [
-                '--disable-web-security',
-                '--disable-features=IsolateOrigins',
-                '--disable-site-isolation-trials',
-                '--no-sandbox',
-                '--remote-debugging-port=9222'
-            ],
-            executablePath: process.platform == "linux" ? '/usr/bin/chromium' : null, // Use an environment variable instead of checking the OS
+        try {
+            // Launch Puppeteer with the bundled Chromium version
+            const browser = await puppeteer.launch({
+                headless: true,  // Run headless
+                defaultViewport: null,  // Use the default viewport
+                args: [
+                    '--disable-web-security',
+                    '--disable-features=IsolateOrigins',
+                    '--disable-site-isolation-trials',
+                    '--no-sandbox',
+                    '--remote-debugging-port=9222'  // Enable remote debugging
+                ]
             });
 
-        console.log("✅ Browser initialised.");
+            console.log("✅ Browser initialised.");
+        } catch (error) {
+            console.error("❌ Error initializing the browser:", error);
+        }
     },
 };
