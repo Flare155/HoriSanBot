e2b48346fc0a5e00fb87b3f421471c0ec5c6b6e4
Flare155 <Otagaisakana@gmail.com>
2024-10-15 23:15:17 -0400

    improve reboot bash command

--- reboot.sh
+++ reboot.sh
@@ -1,3 +1,4 @@
 git pull # Pulls the new changes
 docker-compose down --rmi all # Closes and deletes the old container
-docker-compose up --build -d # Build The new container
+docker-compose up --build -d # Build the new container
+docker compose logs --follow # Follow the console output of the bot instance
