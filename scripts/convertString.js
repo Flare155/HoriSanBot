d354ea3bbab35702594bfa4596b7aeecfc33025c
Flare155 <Otagaisakana@gmail.com>
2024-10-18 17:52:28 -0400

    changed user model, why was it like that???

---
+++ scripts/convertString.js
@@ -0,0 +1,5 @@
+let timestamp = 'Tue Oct 03 2023 22:13:35 GMT+0000 (Coordinated Universal Time)';
+let date = new Date(timestamp);  // Create a Date object from the timestamp
+
+console.log(date.toISOString());  // Output in ISO format
+
