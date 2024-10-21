e76294bbc06d0509f6970291d29c309dc95f5e46
Flare155 <Otagaisakana@gmail.com>
2024-10-20 22:47:56 -0400

    made error messages ephemeral

--- utils/formatting/errorMessageFormatter.js
+++ utils/formatting/errorMessageFormatter.js
@@ -2,7 +2,7 @@
 async function sendErrorMessage(interaction, message) {
     try {
         // Attempt to edit the reply with an error message
-        await interaction.editReply({ content: `\`${message}\`` });
+        await interaction.editReply({ content: `\`${message}\``, ephemeral: true });
         console.error(`Succesfully sent error message: ${message}`);
     } catch (error) {   
         console.error('Failed to send error message for log command');
