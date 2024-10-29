0e1c8334ce6ad8ac5f66ca812d67551179946c6f
tomodutch <4613944+tomodutch@users.noreply.github.com>
2024-10-29 19:46:23 +0100

    Add test for saving the log

--- test/commands/log.test.js
+++ test/commands/log.test.js
@@ -1,9 +1,14 @@
 import { expect, test, vi } from 'vitest';
 import logCommand from '../../commands/global/log';
 
+const saveLog = vi.fn();
 const createMockInteraction = (options = {}) => ({
     options: {
         getString: vi.fn((name) => options[name] || null),
+    },
+    user: {
+        displayName: 'test',
+        displayAvatarURL: () => 'http://example.com/image.jpg'
     },
     reply: vi.fn(), // Mock reply method to capture responses
 });
@@ -16,6 +21,25 @@ test('should fail on invalid time pattern', async () => {
         notes: 'Some notes here',
         episode_length: '30',
     });
-    await logCommand.execute(interaction);
-    expect(interaction.reply).toMatchSnapshot();
+    await logCommand.execute(interaction, saveLog);
+    expectCalledOnceWithSnapshot(interaction.reply);
 });
+
+test('should save log', async () => {
+    const interaction = createMockInteraction({
+        medium: 'Watchtime',
+        amount: '10m',
+        title: 'Test Title',
+        notes: 'Some notes here',
+        episode_length: '30',
+    });
+    await logCommand.execute(interaction, saveLog);
+    expectCalledOnceWithSnapshot(interaction.reply);
+    expect(saveLog).toHaveBeenCalledOnce();
+    expect(saveLog.mock.lastCall[0]).toMatchSnapshot();
+});
+
+const expectCalledOnceWithSnapshot = (obj) => {
+    expect(obj).toHaveBeenCalledOnce();
+    expect(obj).toMatchSnapshot();
+}
