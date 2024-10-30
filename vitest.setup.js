8e3f34ebe933fd493a622d3af960101f8e6829f3
tomodutch <4613944+tomodutch@users.noreply.github.com>
2024-10-30 21:34:32 +0100

    Fix race conditions in setup for tests

--- vitest.setup.js
+++ vitest.setup.js
@@ -1,8 +1,5 @@
 import { afterEach, expect } from 'vitest';
 import * as mongoose from 'mongoose';
-import Log from './models/Log';
-import User from './models/User';
-import Timeout from './models/Timeout';
 
 // replace timestamps in snapshots so they are always the same
 expect.addSnapshotSerializer({
@@ -10,23 +7,14 @@ expect.addSnapshotSerializer({
     print: () => `"TIMESTAMP"`,
 });
 
-// delete all entries in db between each test to prevent state leaking over
-afterEach(async () => {
-    await Promise.all([
-        Log.deleteMany({}),
-        User.deleteMany({}),
-        Timeout.deleteMany({}),
-        // Add more models after they are created here
-    ]);
-});
-
 // https://typegoose.github.io/mongodb-memory-server/docs/guides/integration-examples/test-runners
-beforeAll(async () => {
+beforeEach(async () => {
     // put your client connection code here, example with mongoose:
     await mongoose.connect(process.env['MONGO_URI']);
 });
 
-afterAll(async () => {
+afterEach(async () => {
     // put your client disconnection code here, example with mongoose:
+    // Makes sure each test is run in isolation
     await mongoose.disconnect();
 });
