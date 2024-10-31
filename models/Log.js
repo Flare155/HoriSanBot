7b113d8d508705094e0d3ab16faf461621d3ade0
Flare155 <Otagaisakana@gmail.com>
2024-10-31 05:03:12 -0400

    Added coefficient and games medium for log data model

--- models/Log.js
+++ models/Log.js
@@ -1,23 +1,24 @@
-const mongoose = require('mongoose');
+    const mongoose = require('mongoose');
 
-const logSchema = new mongoose.Schema({
-    userId: { type: String, required: true },
-    guildId: { type: String, required: true },
-    timestamp: { type: Date, required: true },
-    medium: { type: String, required: true, enum: ['Listening', 'Watchtime', 'YouTube', 'Anime', 'Readtime', 'Visual Novel', 'Manga']},
-    title: { type: String, required: true},
-    notes: { type: String, required: false},
-    isBackLog: {type: Boolean, required: true, default: false},
-    amount: {
-        unit: { type: String, required: true, enum: ['Seconds', 'Episodes']},
-        count: { type: Number, required: true, min: 0, max: 72000 },
-        unitLength: { type: Number, required: false, min: 60, max: 72000},
-        totalSeconds: { type: Number, required: true, min: 0, max: 72000},
-    }
-});
+    const logSchema = new mongoose.Schema({
+        userId: { type: String, required: true },
+        guildId: { type: String, required: true },
+        timestamp: { type: Date, required: true },
+        medium: { type: String, required: true, enum: ['Listening', 'Watchtime', 'YouTube', 'Anime', 'Readtime', 'Visual Novel', 'Manga']},
+        title: { type: String, required: true},
+        notes: { type: String, required: false},
+        isBackLog: {type: Boolean, required: true, default: false},
+        amount: {
+            unit: { type: String, required: true, enum: ['Seconds', 'Episodes']},
+            count: { type: Number, required: true, min: 0, max: 72000 },
+            unitLength: { type: Number, required: false, min: 60, max: 72000},
+            coefficient: { type: Number, required: false, min: 0, max: 3 },
+            totalSeconds: { type: Number, required: true, min: 0, max: 72000},
+        }
+    });
 
-logSchema.index({ userId: 1 });
-logSchema.index({ timestamp: -1 });
+    logSchema.index({ userId: 1 });
+    logSchema.index({ timestamp: -1 });
 
-const Log = mongoose.models.Log || mongoose.model('Log', logSchema);
-module.exports = Log;
+    const Log = mongoose.models.Log || mongoose.model('Log', logSchema);
+    module.exports = Log;
