7afe6ee0b75f2c8c6b7957c4ef00e7423c17a0a6
Tsukasa <tsukasasatodesu@gmail.com>
2025-05-01 08:05:16 +0300

    splitted the command into 2 and added user parapetersfor medium, title and notes

--- models/Timer.js
+++ models/Timer.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     userId: { type: String, required: true, unique: true },
-    timerStart: {type: Date, required: true, default: null}
+    timerStart: {type: Date, required: true, default: null},
+    medium: {type: String, required: true}
 });
 
 
